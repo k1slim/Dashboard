2c6a900fb17ffa2d96f621815e42dd3e253a0861
k1slim <k1slim@yandex.ru>
2015-02-25 21:23:33 +0300

    Day 6

--- js/request.js
+++ js/request.js
@@ -5,6 +5,7 @@ define(['jquery', 'jquery-ui'],
             this.weatherFirstImg=$(".drag1FirstImagePlaceholder");
             this.weatherFirst=$(".drag1FirstPlaceholder");
             this.weatherSecond=$(".drag1Second");
+            this.finance=$(".drag4");
         };
 
         Request.prototype.init=function(){
@@ -12,10 +13,12 @@ define(['jquery', 'jquery-ui'],
 
             this.get("http://api.openweathermap.org/data/2.5/weather?q=Minsk&lang=ru&units=metric",this.parseWeather);
             this.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=Minsk&lang=ru&units=metric&cnt=5&mode=xml",this.parseDailyWeather);
+            this.get("https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22USDBYR,EURBYR,RUBBYR%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",this.parseFinance);
 
             setInterval(function(){
                 self.get("http://api.openweathermap.org/data/2.5/weather?q=Minsk&lang=ru&units=metric",self.parseWeather);
                 self.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=Minsk&lang=ru&units=metric&cnt=5&mode=xml",self.parseDailyWeather);
+                self.get("https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22USDBYR,EURBYR,RUBBYR%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",self.parseFinance);
             }, 10800000)
 
         };
@@ -44,6 +47,13 @@ define(['jquery', 'jquery-ui'],
                 $(".drag2Day"+ i+"Title").html(dateArr[i-1][2] + '.' + dateArr[i-1][1]);
                 $(".drag2Day"+ i+"ImagePlaceholder").html('<img src="http://openweathermap.org/img/w/'+$(data).find("symbol")[i-1].getAttribute("var")+'.png" title="'+$(data).find("symbol")[i-1].getAttribute("name")+'"/>');
                 $(".drag2Day"+ i+"Placeholder").html('<strong>' + ~~$(data).find("temperature")[i-1].getAttribute("min") +'&deg...' + ~~$(data).find("temperature")[i-1].getAttribute("max") + '&deg </strong> <br/>' + $(data).find("pressure")[i-1].getAttribute("value")+ ' кПа<br/>'+$(data).find("humidity")[i-1].getAttribute("value")+ '%<br/>'+$(data).find("windSpeed")[i-1].getAttribute("mps")+' м/с' );
+            }
+        };
+
+        Request.prototype.parseFinance=function(data,self){
+            for(var i=0;i<3;i++){
+                $(".buy"+i).html(~~data.query.results.rate[i].Bid+' р.');
+                $(".sell"+i).html(~~data.query.results.rate[i].Ask+' р.');
             }
         };
 
